ffef63d4b07759688bbe20b68d009d1b672525f2
Bryn Bodayle <bryn.bodayle@gmail.com>
2015-05-11 18:05:26 -0500

    Initial commit

---
+++ Example/BABAudioPlayer/BABAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BABAppDelegate.h
+//  BABAudioPlayer
+//
+//  Created by CocoaPods on 05/11/2015.
+//  Copyright (c) 2014 Bryn Bodayle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/BABAudioPlayer/BABViewController.h
@@ -0,0 +1,13 @@
+//
+//  BABViewController.h
+//  BABAudioPlayer
+//
+//  Created by Bryn Bodayle on 05/11/2015.
+//  Copyright (c) 2014 Bryn Bodayle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BABViewController : UIViewController
+
+@end
